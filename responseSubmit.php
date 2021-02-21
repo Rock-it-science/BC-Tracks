39bc014fddc2c8624e75edd3b65d97f0e5850d33
Will <wilmc17@gmail.com>
2021-02-20 19:12:46 -0800

    reduction for testing

--- responseSubmit.php
+++ responseSubmit.php
@@ -4,11 +4,12 @@ echo("submitting response");
 //Submit response to database
 $response = $_REQUEST["response"];
 
-$conn = new mysqli($_ENV["db_servername"], $_ENV["db_username"], $_ENV["db_password"], $_ENV["db_name"]);
+//$conn = new mysqli($_ENV["db_servername"], $_ENV["db_username"], $_ENV["db_password"], $_ENV["db_name"]);
 
-if($conn->connect_error){
+/*if($conn->connect_error){
   die("Connection failed: ". $conn->connect_error);
-}
+}*/
+echo($_ENV["db_servername");
 
 echo($response);
 /*$sql = "INSERT INTO responses VALUES ('$response')";
