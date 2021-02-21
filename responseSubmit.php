11452c58baafced98ab103e05bbc3d7c3e148cf7
Will <wilmc17@gmail.com>
2021-02-20 19:21:50 -0800

    try to get something working

--- responseSubmit.php
+++ responseSubmit.php
@@ -2,16 +2,16 @@
 echo("submitting response");
 
 //Submit response to database
-$response = $_REQUEST["response"];
+//$response = $_REQUEST["response"];
 
 //$conn = new mysqli($_ENV["db_servername"], $_ENV["db_username"], $_ENV["db_password"], $_ENV["db_name"]);
 
 /*if($conn->connect_error){
   die("Connection failed: ". $conn->connect_error);
 }*/
-echo($_ENV["db_servername");
+//echo($_ENV["db_servername");
 
-echo($response);
+//echo($response);
 /*$sql = "INSERT INTO responses VALUES ('$response')";
 if($conn->query($sql) === TRUE){
   echo "report submitted successfully";
