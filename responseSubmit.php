ec47efada3651b7cfeaeb17311c9f1a153fb6f35
Will <wilmc17@gmail.com>
2021-02-20 19:32:23 -0800

    adding species and env var

--- responseSubmit.php
+++ responseSubmit.php
@@ -2,16 +2,16 @@
 echo("submitting response");
 
 //Submit response to database
-$response = $_REQUEST["response"];
+$species = $_REQUEST["species"];
 
 //$conn = new mysqli($_ENV["db_servername"], $_ENV["db_username"], $_ENV["db_password"], $_ENV["db_name"]);
 
 /*if($conn->connect_error){
   die("Connection failed: ". $conn->connect_error);
 }*/
-//echo($_ENV["db_servername");
+echo($_ENV["db_servername");
 
-echo($response);
+echo($species);
 /*$sql = "INSERT INTO responses VALUES ('$response')";
 if($conn->query($sql) === TRUE){
   echo "report submitted successfully";
