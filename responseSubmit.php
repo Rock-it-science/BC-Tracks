8ca9ed97aadd16ace253b8a19e352b73392f84b5
Will <wilmc17@gmail.com>
2021-02-21 02:41:54 -0800

    Fixy

--- responseSubmit.php
+++ responseSubmit.php
@@ -6,7 +6,7 @@ print_r($_POST);
 
 //Column logic
 $animal = $_POST["animal"];
-if(!empty($_POST["animal_other"])){
+if()$_POST["animal_other"] == "Other"){
   $animal = $POST["animal_other"];
 }
 
@@ -40,12 +40,12 @@ $sql = "INSERT INTO reports VALUES (
 echo($sql);
 
 // Execute Query
-//pg_query($sql) or die('Query failed: ' . pg_last_error());
+pg_query($sql) or die('Query failed: ' . pg_last_error());
 
 pg_close($conn);
 
 // Redirect back to main page
-//header("Location: index.html");
+header("Location: index.html");
 die();
 
 
