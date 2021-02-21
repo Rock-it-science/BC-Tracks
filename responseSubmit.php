5b8024f355844b7da2cfcc777d8ce9de7c4a793b
Will <wilmc17@gmail.com>
2021-02-20 21:35:57 -0800

    removing bad ;

--- responseSubmit.php
+++ responseSubmit.php
@@ -3,9 +3,10 @@ echo("submitting response");
 
 //Submit response to database
 print_r($_POST);
+echo('host=' . $_ENV["db_servername"] . '  dbname=' . $_ENV["db_name"] . ' user=' . $_ENV["db_username"] . ' password=' . $_ENV["db_password"]);
 
-/*$conn = pg_connect('host=' . $_ENV["db_servername"] . '  dbname=' . $_ENV["db_name"] . ' user=' . $_ENV["db_username"] . ' password=' . $_ENV["db_password"]);
- or die("Connection failed: " . pg_last_error());*/
+$conn = pg_connect('host=' . $_ENV["db_servername"] . '  dbname=' . $_ENV["db_name"] . ' user=' . $_ENV["db_username"] . ' password=' . $_ENV["db_password"])
+ or die("Connection failed: " . pg_last_error());
 
 /*$sql = "INSERT INTO responses VALUES ('$response')";
 if($conn->query($sql) === TRUE){
@@ -16,5 +17,5 @@ if($conn->query($sql) === TRUE){
   echo "Error" . $sql . "<br>" . $conn->error;
 }
 */
-//pg_close($conn);
+pg_close($conn);
  ?>
