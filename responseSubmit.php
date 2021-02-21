5aece3ad86b538fc897e97adfaf81c8ba661cbc2
Will <wilmc17@gmail.com>
2021-02-20 23:49:26 -0800

    formatting for sql

--- responseSubmit.php
+++ responseSubmit.php
@@ -4,48 +4,47 @@ echo("submitting response");
 //Submit response to database
 print_r($_POST);
 
-echo("\n" . $_COOKIE['long'] . ", " . $_COOKIE['lat'] . ", " . date('Y-n-j H:i:s') .",
+/*echo("\n" . $_COOKIE['long'] . ", " . $_COOKIE['lat'] . ", " . date('Y-n-j H:i:s') .",
   ". $_POST['animal'] .",
   ". $_POST['species'] .",
   ". $_POST['notes'] .",
   ". $_POST['name'] .",
   ". $_POST['email']);
-flush();
+flush();*/
 
 //Column logic
-$risk = false;
+$risk = "No";
 if($_POST["risk"] == "on"){
-  $risk = true;
+  $risk = "Yes";
 }
 
-$distress = false;
+$distress = "No";
 if($_POST["distress"] == "on"){
-  $distress = true;
+  $distress = "Yes";
 }
 
-echo(", " . $risk . ", " . $distress);
+//echo(", " . $risk . ", " . $distress);
 
-/*$conn = pg_connect('host=' . $_ENV["db_servername"] . '  dbname=' . $_ENV["db_name"] . ' user=' . $_ENV["db_username"] . ' password=' . $_ENV["db_password"])
+$conn = pg_connect('host=' . $_ENV["db_servername"] . '  dbname=' . $_ENV["db_name"] . ' user=' . $_ENV["db_username"] . ' password=' . $_ENV["db_password"])
  or die("Connection failed: " . pg_last_error());
-*/
 
 
-/*$sql = "INSERT INTO responses VALUES (
+
+$sql = "INSERT INTO responses VALUES (
   (SELECT MAX(qc_id) + 1 FROM reports),
   ST_GEOMFROMTEXT(\"MULTIPOINT(" . $_COOKIE['long'] . " " . $_COOKIE['lat'] . "\"),
-  ". $Risk .",
-  ". $distress .",
-  DATETIME(".time().") .",
-  ". $_POST['Animal'] .",
-  ". $_POST['Species'] .",
-  ". $_POST['Notes'] .",
-  ". $_POST['Name'] .",
-  ". $_POST['Email']"
+  \'". $risk ."\',
+  \'". $distress ."\',
+  DATETIME(".date('Y-n-j H:i:s').") .",
+  \'". $_POST['animal'] ."\',
+  \'". $_POST['species'] ."\',
+  \'". $_POST['notes'] ."\',
+  \'". $_POST['name'] ."\',
+  \'". $_POST['email']"\'
 )";
 
-echo($sql);*/
+echo($sql);
 
-/*
 if($conn->query($sql) === TRUE){
   echo "report submitted successfully";
   header('Location: success.html');
@@ -53,6 +52,7 @@ if($conn->query($sql) === TRUE){
 } else{
   echo "Error" . $sql . "<br>" . $conn->error;
 }
-*/
-//pg_close($conn);
- ?>
+
+pg_close($conn);
+
+?>
